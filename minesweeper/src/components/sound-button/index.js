af65e10394de7db87d88b5345ab3b18d39638130
Quaqva <sanek15056@gmail.com>
2023-05-23 17:55:21 +0400

    feat: change button image

--- minesweeper/src/components/sound-button/index.js
+++ minesweeper/src/components/sound-button/index.js
@@ -1,6 +1,6 @@
 import buttonLayout from './index.html';
 import './styles.scss';
-import soundImage from '../../assets/img/sound.jpg';
+import soundImage from '../../assets/img/sound.png';
 import noSoundImage from '../../assets/img/no-sound.png';
 import createElement from '../../utils/createElement';
 
