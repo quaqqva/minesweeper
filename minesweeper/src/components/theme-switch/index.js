86867ff7c8df4616b4d69bb82a70ead0739cee67
Quaqva <sanek15056@gmail.com>
2023-05-23 15:43:35 +0400

    feat: add changing theme logic

--- minesweeper/src/components/theme-switch/index.js
+++ minesweeper/src/components/theme-switch/index.js
@@ -1,8 +1,14 @@
 import './styles.scss';
 
+function changeTheme(element) {
+  element.classList.toggle('night');
+  Array.from(element.children).forEach((child) => changeTheme(child));
+}
+
 export default function createThemeSwitch() {
   const input = document.createElement('input');
   input.type = 'checkbox';
   input.className = 'theme-switch';
+  input.addEventListener('input', changeTheme.bind(null, document.body));
   return input;
 }
